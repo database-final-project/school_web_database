ffdc51f2126b76790437363ddad6ed188de5ee94
B Bennett <Brockbennett2324@gmail.com>
2019-04-28 16:48:04 -0400

    teacher portal

---
+++ teacher-portal.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title> Teacher-Portal </title>
+    <link rel="stylesheet" href="style-teacher-portal.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+  </head>
+  <body>
+
+    <div class="studentProfile">
+      <div class="profileImg"> </div>
+      <h1> Teacher Name  </h1>
+      <h1> teacher Id  </h1>
+    </div>
+
+      <div class="middle">
+        <div class="menu">
+          <li class="item" id='student'>
+            <a href="#student" class="btn"><i class="fas fa-book-open"></i> Classroom Information </a>
+            <div class="smenu">
+              <a href="#"> View Schedule </a>
+              <a href="#"> Update Student Attendance </a>
+              <a href="#"> Update Student Grade  </a>
+            </div>
+          </li>
+
+
+          <li class="item">
+            <a class="btn" href="logout.php"><i class="fas fa-sign-out-alt"></i> Logout</a>
+            </div>
+          </li>
+
+
+        </div>
+      </div>
+  </body>
+</html>
