e6519cb37ee2c97e69f88f41c95f96ad3ce3814b
SemHamza <99shamza@gmail.com>
2019-04-28 16:16:17 -0400

    Update studentGrades.php
    Fixed the student grades table

--- studentGrades.php
+++ studentGrades.php
@@ -15,37 +15,44 @@
       </table>
 
     <?php
+	session_start();
 
-
-    @ $db = new mysqli('localhost', 'root', 'gigigino10!', 'k_12_school');
+    @ $db = new mysqli('localhost', 'root', '12345', 'k_12_school');
 
     if (mysqli_connect_errno()) {
        echo 'Error: Could not connect to database.  Please try again later.';
        exit;
     }
+	
+	$student_id = $_SESSION["student_id"];
+	
+	if (!get_magic_quotes_gpc()){
+		$student_id = addslashes($student_id);
+	  }
 
-    $query = "SELECT course_name  as 'Course Name', grade as 'Letter Grade'
+    $query = "SELECT course_name, grade
            FROM course c
            join enrolls_in ei on c.course_id = ei.course_id
            join student s on ei.student_id = s.student_id
-           where s.student_id = '101'
-           ";
+           where s.student_id = " .$student_id;
 
     $result = $db->query($query);
-
     $num_results = $result->num_rows;
-
+	
     if ($num_results > 0) {
-      while ($row = $result-> fetch_assoc() ) {
+	  echo "<table>";
+	  
+	  while ($row = $result-> fetch_assoc() ) {
         echo "<tr><td>". $row["course_name"]. "</td><td>". $row["grade"]. "</td></tr>";
         }
+		
       echo "</table>";
     }
      else {
       echo "0 results";
     }
-
-    $conn-> close();
+	
+    $db->close();
     ?>
 
 
