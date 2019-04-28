6301d6fff685c2c810bdf970c1ba5c1b8cdfff67
jussepen <jussepen@gmail.com>
2019-04-28 18:02:07 -0400

    display the student's schedule

---
+++ studentSchedule.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title> Student Grades </title>
+    <link rel="stylesheet" href="style-studentGrades.css">
+  </head>
+  <body>
+
+      <table>
+        <tr>
+          <th> Student Class </th>
+          <th> Class Time </th>
+        </tr>
+      </table>
+
+    <?php
+	session_start();
+
+    @ $db = new mysqli('localhost', 'root', '12345', 'k_12_school');
+
+    if (mysqli_connect_errno()) {
+       echo 'Error: Could not connect to database.  Please try again later.';
+       exit;
+    }
+
+	$student_id = $_SESSION["student_id"];
+
+	if (!get_magic_quotes_gpc()){
+		$student_id = addslashes($student_id);
+	  }
+
+    $query = "SELECT course_name, course_schedule
+              FROM course c
+              join enrolls_in ei on c.course_id = ei.course_id
+              join student s on ei.student_id = s.student_id
+              where s.student_id " = .$student_id;
+
+    $result = $db->query($query);
+    $num_results = $result->num_rows;
+
+    if ($num_results > 0) {
+	  echo "<table>";
+
+	  while ($row = $result-> fetch_assoc() ) {
+        echo "<tr><td>". $row["course_name"]. "</td><td>". $row["course_schedule"]. "</td></tr>";
+        }
+
+      echo "</table>";
+    }
+     else {
+      echo "0 results";
+    }
+
+    $db->close();
+    ?>
+
+
+  </body>
+</html>
