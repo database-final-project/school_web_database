6412a6f914067e2c3badeb85ad42c22bf0c46929
jussepen <jussepen@gmail.com>
2019-04-29 01:18:36 -0400

    tables
    fixed table aligiment

--- cafeteriaMenu.php
+++ cafeteriaMenu.php
@@ -13,7 +13,6 @@
           <th> breakfast </th>
           <th> Lunch </th>
         </tr>
-      </table>
 
     <?php
 	session_start();
@@ -87,7 +86,6 @@
     $row_fb = $result_fb-> fetch_assoc();
     $row_fl = $result_fl-> fetch_assoc();
 
-	  echo "<table class= 'cafeteriaMenu'>";
 
     echo "<tr><td> Monday </td><td>". $row_mb["food_name"]. "</td><td>". $row_ml["food_name"]. "</td></tr>";
     echo "<tr><td> Tuesday </td><td>". $row_tb["food_name"]. "</td><td>". $row_tl["food_name"]. "</td></tr>";
